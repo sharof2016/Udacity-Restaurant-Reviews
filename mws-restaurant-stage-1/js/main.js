968a668c0afb33f7d13dc59dace5b4b6c95eb730
sharof2016 <akhrorovs2010@gmail.com>
2018-11-29 02:00:08 -0800

    Update main.js

--- mws-restaurant-stage-1/js/main.js
+++ mws-restaurant-stage-1/js/main.js
@@ -59,7 +59,7 @@ fetchCuisines = () => {
 */
 fillCuisinesHTML = (cuisines = self.cuisines) => {
   const select = document.getElementById('cuisines-select');
-
+  
   cuisines.forEach(cuisine => {
     const option = document.createElement('option');
     option.innerHTML = cuisine;
@@ -85,7 +85,7 @@ initMap = () => {
     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets'
   }).addTo(newMap);
-
+  
   updateRestaurants();
 }
 /* window.initMap = () => {
@@ -107,13 +107,13 @@ updateRestaurants();
 updateRestaurants = () => {
   const cSelect = document.getElementById('cuisines-select');
   const nSelect = document.getElementById('neighborhoods-select');
-
+  
   const cIndex = cSelect.selectedIndex;
   const nIndex = nSelect.selectedIndex;
-
+  
   const cuisine = cSelect[cIndex].value;
   const neighborhood = nSelect[nIndex].value;
-
+  
   DBHelper.fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, (error, restaurants) => {
     if (error) { // Got an error!
       console.error(error);
@@ -132,7 +132,7 @@ resetRestaurants = (restaurants) => {
   self.restaurants = [];
   const ul = document.getElementById('restaurants-list');
   ul.innerHTML = '';
-
+  
   // Remove all map markers
   if (self.markers) {
     self.markers.forEach(marker => marker.remove());
@@ -157,29 +157,30 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-
+  
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute("alt", `Picture of restaurant ${restaurant.name}`);
   li.append(image);
-
+  
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
   li.append(name);
-
+  
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
   li.append(neighborhood);
-
+  
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
   li.append(address);
-
+  
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
-
+  
   return li
 }
 
@@ -196,7 +197,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     }
     self.markers.push(marker);
   });
-
+  
 }
 /* addMarkersToMap = (restaurants = self.restaurants) => {
 restaurants.forEach(restaurant => {
